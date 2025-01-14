42d1fd6bb950ee0ae3f3457405adeb15e6f6fefb
Arenacloserr <162641455+Arenacloserr@users.noreply.github.com>
2025-01-14 18:18:51 -0500

    Talon code written

--- src/main/java/frc/robot/Drive/ModuleIO.java
+++ src/main/java/frc/robot/Drive/ModuleIO.java
@@ -13,8 +13,9 @@ public interface ModuleIO {
     public double driveAppliedVolts = 0.0;
     public double[] driveCurrentAmps = new double[] {};
 
-    public Rotation2d turnbsolutePosition = new Rotation2d();
+    public Rotation2d turnAbsolutePosition = new Rotation2d();
     public Rotation2d turnPosition = new Rotation2d();
+    public double turnAppliedVolts = 0.0;
     public double turnVelocityRadPerSec = 0.0;
     public double turnAppliedRadPerSec = 0.0;
     public double[] turnCurrentAmps = new double[] {};
