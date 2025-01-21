d0b59e6f31d2a817803d75076ae42f6043d3fe29
elsaroni <150392934+elsaroni@users.noreply.github.com>
2025-01-20 22:17:27 -0500

    elevator sim (might need to fix)

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -15,19 +15,31 @@ import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 public class ElevatorIOSim implements ElevatorIO {
     private ElevatorSim elevatorSim = new ElevatorSim(0, 0, null, 0, 0, false, 0, null);
 
-    private double ElevatorAppliedVolts = 0.0;
+    private double elevatorAppliedVolts = 0.0;
 
 
+@Override 
+public void updateInputs(ElevatorIOInputs inputs) {
+    elevatorSim.update(ElevatorConstants.kDt);
+    inputs.elevatorPositionRads = elevatorSim.getAngleRads();
+    inputs.elevatorMotorPositionRads = elevatorSim.getAngleRads();
+    inputs.elevatorMotorVelocityRadPerSec = elevatorSim.getVelocityRadPerSec();
+    inputs.elevatorCurrent = elevatorSim.getCurrentDrawAmps();
+    inputs.elevatorAppliedVolts = elevatorAppliedVolts;
 
 @Override
 public void moveElevator(double speed) {
     ElevatorAppliedVolts = MathUtil.clamp(speed, speed, speed);
+    elevatorSim.setInputVoltage(elevatorAppliedVolts)
   
 }
 
 @Override
 public void setVoltage(double volts){
     ElevatorAppliedVolts = volts;
+    Logger.recordOutput("Setting output", volts);
+        elevatorSim.setInputVoltage(elevatorAppliedVolts - (voltage, voltage, voltage));
 }
         
+    }
 }
