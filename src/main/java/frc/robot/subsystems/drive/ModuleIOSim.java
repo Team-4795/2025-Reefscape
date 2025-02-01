282a2d04fb5143b0c6899f927c18f77ff7ff225c
MikhailSaeed <mikhailnedal@gmail.com>
2025-02-01 10:56:31 -0500

    Code cleanup and variables change

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
@@ -8,34 +8,35 @@ import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ModuleIOSim implements ModuleIO {
     private static final double LOOP_PERIOD_SECS = 0.02;
-    //change gearbox argument
-    private DCMotorSim driveSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60(4),  0.025, DriveConstants.DriveGearing), DCMotor.getKrakenX60(4)); //needs to change
-    private DCMotorSim turnSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getNeoVortex(4), 0.004, DriveConstants.TurnGearing), DCMotor.getNeoVortex(4)); //needs to change
+    // change gearbox argument
+    private DCMotorSim driveSim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60(4), 0.025, DriveConstants.DriveGearing),
+            DCMotor.getKrakenX60(4)); // needs to change
+    private DCMotorSim turnSim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(DCMotor.getNeoVortex(4), 0.004, DriveConstants.TurnGearing),
+            DCMotor.getNeoVortex(4)); // needs to change
 
-    private final Rotation2d turnAbsoluteInitPositon = new Rotation2d(Math.random()*2.0*Math.PI);
+    private final Rotation2d turnAbsoluteInitPositon = new Rotation2d(Math.random() * 2.0 * Math.PI);
     private double driveAppliedVolts = 0.0;
     private double turnAppliedVolts = 0.0;
 
     @Override
-    public void updateInputs(ModuleIOInputs inputs){
+    public void updateInputs(ModuleIOInputs inputs) {
         driveSim.update(LOOP_PERIOD_SECS);
         turnSim.update(LOOP_PERIOD_SECS);
 
         inputs.drivePositionRad = driveSim.getAngularPositionRad();
         inputs.driveVelocityRadPerSec = driveSim.getAngularVelocityRadPerSec();
         inputs.driveAppliedVolts = driveAppliedVolts;
-        inputs.driveCurrentAmps = new double[] {Math.abs(driveSim.getCurrentDrawAmps())};
-
-
-        inputs.turnAbsolutePosition =
-            new Rotation2d(turnSim.getAngularPositionRad()).plus(turnAbsoluteInitPositon);
+        inputs.driveCurrentAmps = new double[] { Math.abs(driveSim.getCurrentDrawAmps()) };
+        inputs.turnAbsolutePosition = new Rotation2d(turnSim.getAngularPositionRad()).plus(turnAbsoluteInitPositon);
         inputs.turnPosition = new Rotation2d(turnSim.getAngularPosition());
         inputs.turnAppliedVolts = turnAppliedVolts;
-        inputs.turnCurrentAmps = new double[] {Math.abs(turnSim.getCurrentDrawAmps())};
+        inputs.turnCurrentAmps = new double[] { Math.abs(turnSim.getCurrentDrawAmps()) };
     }
 
     @Override
-    public void setDriveVoltage(double volts){
+    public void setDriveVoltage(double volts) {
         driveAppliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
         driveSim.setInputVoltage(driveAppliedVolts);
     }
