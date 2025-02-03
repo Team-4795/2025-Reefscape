7b68c62aa079279360e2eadc42eb49efc2f215ab
MikhailSaeed <mikhailnedal@gmail.com>
2025-02-03 15:19:51 -0500

    Code Cleanup

--- src/main/java/frc/robot/subsystems/drive/ModuleIO.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIO.java
@@ -13,26 +13,32 @@ public interface ModuleIO {
     public double driveVelocityRadPerSec = 0.0;
     public double driveAppliedVolts = 0.0;
     public double[] driveCurrentAmps = new double[] {};
-
     public Rotation2d turnAbsolutePosition = new Rotation2d();
     public Rotation2d turnPosition = new Rotation2d();
     public double turnAppliedVolts = 0.0;
     public double turnVelocityRadPerSec = 0.0;
     public double turnAppliedRadPerSec = 0.0;
     public double[] turnCurrentAmps = new double[] {};
-  }  
+  }
 
-  public default void updateInputs(ModuleIOInputs inputs) {}
+  public default void updateInputs(ModuleIOInputs inputs) {
+  }
 
-  public default void setDriveVoltage(double volts) {}
+  public default void setDriveVoltage(double volts) {
+  }
 
-  public default void runTalonPID (double desiredStateRotPerSecond) {}
+  public default void runMovementPID(double desiredStateRotPerSecond, double rotationDesiredPosition) {
+  }
 
-  public default void setDesiredState(SwerveModuleState state) {}
+  public default void setDesiredState(SwerveModuleState state) {
+  }
 
-  public default void setTurnVoltage (double volts) {}
+  public default void setTurnVoltage(double volts) {
+  }
 
-  public default void setDriveBrakeMode(boolean enable) {}
+  public default void setDriveBrakeMode(boolean enable) {
+  }
 
-  public default void setTurnBrakeMode(boolean enable) {}
-} 
+  public default void setTurnBrakeMode(boolean enable) {
+  }
+}
