f77a6764f69525a9433c9551f208082cbb8cb850
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2025-02-12 11:34:50 -0500

    merge clean up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -109,7 +109,34 @@ public class RobotContainer {
     configureBindings();
   }
 
-  private void configureBindings() {
+  private void configureBindings() {{
+      // Note that X is defined as forward according to WPILib convention,
+      // and Y is defined as to the left according to WPILib convention.
+      drivetrain.setDefaultCommand(
+          // Drivetrain will execute this command periodically
+          drivetrain.applyRequest(() ->
+              drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                  .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                  .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+          )
+      );
+  
+      joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+      joystick.b().whileTrue(drivetrain.applyRequest(() ->
+          point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+      ));
+  
+      // Run SysId routines when holding back/start and X/Y.
+      // Note that each routine should be run exactly once in a single log.
+      joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+      joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+      joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+      joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+  
+      // reset the field-centric heading on left bumper press
+      joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+  
+      drivetrain.registerTelemetry(logger::telemeterize);
     // drive.setDefaultCommand(
     //     DriveCommands.joystickDrive(
     //         drive,
@@ -214,6 +241,7 @@ public class RobotContainer {
       OIConstants.driverController.leftBumper().onTrue(Commands.runOnce(() -> Arm.getInstance().setGoal(-1.8)));
       OIConstants.driverController.rightBumper().onTrue(Commands.runOnce(() -> Arm.getInstance().setGoal(ArmConstants.CORAL_L2)));
   }
+}
 
   public Command getAutonomousCommand() {
     return Commands.print("hi");
