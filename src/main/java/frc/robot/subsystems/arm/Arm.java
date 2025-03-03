b11bdbff290dea4a6ba63fd9c11e907e6f984367
ozziexyz <ozzieuh@gmail.com>
2025-03-03 09:17:46 -0500

    update motion profile in arm default command

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -52,6 +52,7 @@ public class Arm extends SubsystemBase {
                 double down = MathUtil.applyDeadband(Math.pow(OIConstants.operatorController.getRightTriggerAxis(), 3), 0.1);
                 double change = (up - down) * 0.02;
                 // io.setGoal(inputs.goalAngle + change);
+                io.updateMotionProfile();
             }, this)
         );
         // setDefaultCommand(
