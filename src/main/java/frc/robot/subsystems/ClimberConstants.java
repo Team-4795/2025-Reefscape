e7127c975b67f8ba1062181ee23bfaa2ddbed33d
MikhailSaeed <mikhailnedal@gmail.com>
2025-03-26 19:07:09 -0400

    added a couple constants

--- src/main/java/frc/robot/subsystems/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/ClimberConstants.java
@@ -2,4 +2,14 @@ package frc.robot.subsystems;
 
 public class ClimberConstants {
     
+    public static double ClimberID = 9;
+
+    public static double gearing = 250;
+
+
+    public class SimConstants {
+    
+        
+    }
+
 }
